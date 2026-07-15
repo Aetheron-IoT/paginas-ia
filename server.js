268a92c03f40235344db05ac968a701ad9087cbd
Luis Figueroa <luis_figueroa_morales@yahoo.com>
2026-07-14 22:12:26 -0500

    express a la orden

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { GoogleGenAI } = require('@google/genai');
 
 const app = express();
-app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static(__dirname));
 app.use(cors());
 
 if (!process.env.GEMINI_API_KEY) {
